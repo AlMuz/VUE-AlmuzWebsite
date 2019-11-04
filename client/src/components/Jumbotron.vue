6c95b822125da6460a7251cc02f07981afdba410
AlMuz <almuz@inbox.lv>
2019-11-04 22:23:15 +0200

    Jumbotron and custom navbar

---
+++ client/src/components/Jumbotron.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <b-jumbotron>
+      <b-row>
+        <b-col md="4" cols="12">
+            <b-img src="@/assets/images/me.png" fluid alt="Responsive image" rounded="circle" class="mx-auto d-block"></b-img>
+        </b-col>
+        <b-col md="7" offset-md="1">
+          <h1>Aleksejs Muzicenko</h1>
+          <p>Programmer</p>
+        </b-col>
+      </b-row>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+
+export default {
+};
+</script>
+<style>
+.img-center {margin:0 auto;}
+</style>
