cd03aac6435e57ac2f6306901db045f9074b01dc
AlMuz <almuz@inbox.lv>
2019-11-04 22:22:46 +0200

    Clear code

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/mevn.jpg" width="350" />
-    <HelloWorld msg="Basic Boilerplate Template" />
+    <h2>Home</h2>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
 };
 </script>
