4b1e457a0bf69adb84bc8acef56654bbdb7654cb
AlMuz <almuz@inbox.lv>
2019-11-05 23:05:36 +0200

    Jumbotron done and some fixes

--- client/src/components/Jumbotron.vue
+++ client/src/components/Jumbotron.vue
@@ -1,24 +1,25 @@
 <template>
-  <div>
-    <b-jumbotron>
-      <b-row>
-        <b-col md="4" cols="12">
-            <b-img src="@/assets/images/me.png" fluid alt="Responsive image" rounded="circle" class="mx-auto d-block"></b-img>
-        </b-col>
-        <b-col md="7" offset-md="1">
-          <h1>Aleksejs Muzicenko</h1>
-          <p>Programmer</p>
-        </b-col>
-      </b-row>
-    </b-jumbotron>
-  </div>
+  <b-jumbotron>
+    <b-row>
+      <b-col md="4" cols="12">
+          <b-img src="@/assets/images/me.png" fluid alt="Responsive image" rounded="circle" class="mx-auto d-block"></b-img>
+      </b-col>
+      <b-col md="7" offset-md="1">
+        <h1>{{ $t("jumbotron.nameAndSurname") }}</h1>
+        <h2>{{ $t("jumbotron.position") }}</h2>
+      </b-col>
+    </b-row>
+  </b-jumbotron>
 </template>
 
 <script>
-
-export default {
-};
+  export default {
+  };
 </script>
+
 <style>
-.img-center {margin:0 auto;}
+  .jumbotron {
+    margin-bottom: 0!important;
+    border-radius: 0!important;
+  }
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h2>Home</h2>
+    <h3>Home</h3>
   </div>
 </template>
 
