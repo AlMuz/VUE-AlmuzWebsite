b341a663f918cc6be669ac9a5c41376d4050a1fa
AlMuz <almuz@inbox.lv>
2019-11-10 23:42:26 +0200

    Chat - done

--- client/src/layouts/Conversation.vue
+++ client/src/layouts/Conversation.vue
@@ -6,10 +6,10 @@
         <b-row v-for="(value, index) in chat" :key="index" :class="value.person">
           <b-col md="4">
             <div class="bubble">
-              <p v-if="value.text" v-html="value.text"></p>
-              <p v-if="value.translation">{{ $t("conversation."+value.translation) }}</p>
-              <b-button v-if="value.languages" v-for="language in value.languages" variant="danger" v-on:click="languageSelected(language)" class="mx-4">{{language}}</b-button>
-              <b-input v-if="value.input && value.input == 'name'" id="inline-form-input" :placeholder="$t('conversation.name')" v-model="nameInput" v-on:keyup.enter="submitName"></b-input>
+              <p v-html="renderText(value)"></p>
+              <b-button v-if="value.languages" v-for="language in value.languages" variant="primary" v-on:click="languageSelected(language)" class="mx-4">{{language | capitalize}}</b-button>
+              <b-input v-if="value.input && value.input == 'name'" id="inline-form-input" :placeholder="$t('conversation.name')" v-model="nameInput" v-on:keyup.enter="submitNameInput" :disabled="isDisabled"></b-input>
+              <b-button v-if="value.smiles" v-for="smile in value.smiles" variant="success" v-on:click="smileSelected" class="mx-4" v-html="smile"></b-button>
             </div>
           </b-col>
         </b-row>
@@ -25,17 +25,19 @@ export default {
   data() {
     return {
       nameInput: '',
+      nameInputFilled: false,
       preDefinedChat: [
         {step: 1, person: "robot", text: "Hello"},
         {step: 1, person: "robot", text: "What language do you prefer?"},
-        {step: 1, person: "information", text: "Languages to choose:"},
+        {step: 1, person: "information", text: "Languages to choose"},
         {step: 1, person: "information", languages: this.$store.getters.getLanguages},
         {step: 2, person: "information", translation: "choosedLanguage"},
         {step: 2, person: "robot", translation: "great"},
         {step: 2, person: "robot", translation: "nameYourself"},
         {step: 2, person: "person", input: "name"},
-        {step: 3, person: "robot", text: "Dummy text"},
-        {step: 3, person: "robot", text: "Dummy text"},
+        {step: 3, person: "robot", translation: "niceToMeetYou", "additionalTranslation": true, "additionalTranslationData": "nameInput"},
+        {step: 3, person: "robot", text: "&#128075;"},
+        {step: 3, person: "person", smiles: ["&#128075;", "&#128074;", "&#128077;"]},
       ],
       chat: [],
       selectedLanguage: null
@@ -56,19 +58,20 @@ export default {
         this.renderStep(2);
       }, 1000);
     },
+
     setTimeoutFunction(key, value) {
       setTimeout(() => {
         this.chat.push(value)
       }, 1000 * key);
     },
-    submitName() {
-      this.$store.dispatch('changeVisitorName', this.nameInput);
-      const thirdStep = this.preDefinedChat.filter(value => value.step == 3);
-
-      setTimeout(() => {
-        this.renderStep(3);
-      }, 1000);
-      // this.$store.dispatch('skipIntro');
+    renderText(value) {
+      if (value.text) {
+        return value.text;
+      } else if (value.additionalTranslationData && value.translation) {
+        return this.$t(`conversation.${value.translation}`, {'name': this[value.additionalTranslationData]});
+      } else if (value.translation) {
+        return this.$t(`conversation.${value.translation}`);
+      }
     },
     renderStep(step) {
       const stepsData = this.preDefinedChat.filter(value => value.step == step);
@@ -78,8 +81,26 @@ export default {
       stepsData.forEach(function(item, i) {
         that.setTimeoutFunction(i, item);
       })
+    },
+    smileSelected(){
+      setTimeout(() => {
+        this.$store.dispatch('skipIntro');
+      }, 1000);
+    },
+    submitNameInput() {
+      this.$store.dispatch('changeVisitorName', this.nameInput);
+      this.nameInputFilled = true;
+
+      setTimeout(() => {
+        this.renderStep(3);
+      }, 1000);
     }
   },
+  computed: {
+    isDisabled() {
+      return this.nameInputFilled;
+    }
+   },
   created() {
     this.renderStep(1);
   }
@@ -166,7 +187,7 @@ export default {
       transform: translateY(0);
     }
   }
-  
+
   @keyframes slide-out {
     from {
       transform: translateY(0);
