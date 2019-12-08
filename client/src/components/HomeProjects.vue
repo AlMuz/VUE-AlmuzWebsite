fc3f38bae10cef761f7b4163cec46cca3dde15e3
AlMuz <almuz@inbox.lv>
2019-12-08 23:36:47 +0200

    Projects on home page

---
+++ client/src/components/HomeProjects.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="projects">
+    <h3>{{ $t("mainPage.projects") }}</h3>
+    <div class="loaded" v-if="!loading">
+      <div class="table-responsive">
+        <table role="table" aria-busy="false" aria-colcount="2" class="table b-table">
+          <thead role="rowgroup">
+            <tr role="row">
+              <th role="columnheader" scope="col" aria-colindex="1"> {{ this.$t("mainPage.name") }} </th>
+              <th role="columnheader" scope="col" aria-colindex="2"> {{ this.$t("mainPage.language") }} </th>
+              <th role="columnheader" scope="col" aria-colindex="3"> {{ this.$t("mainPage.link") }} </th>
+              <th role="columnheader" scope="col" aria-colindex="4"> {{ this.$t("mainPage.created") }} </th>
+              <th role="columnheader" scope="col" aria-colindex="5"> {{ this.$t("mainPage.updated") }} </th>
+            </tr>
+          </thead>
+          <tbody role="rowgroup">
+            <tr role="row" v-for="project in projects">
+              <td role="cell" v-for="(data, name) in project" :class="name">
+                <a :href="data" target="_blank" v-if="name == 'link'">
+                  <i class="fab fa-github"></i>
+                </a>
+                <p v-else>
+                  {{ data }}
+                </p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="loader text-center" v-else>
+      <i class="fas fa-5x fa-spin fa-spinner"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ["loading", "projects"]
+}
+</script>
+
+<style lang="css" scoped>
+thead tr {
+  text-align: center;
+}
+.link {
+  text-align: center;
+}
+.link a {
+  font-size: 30px;
+}
+.link a .fab {
+  color: #272643;
+}
+.link a .fab:hover {
+  color: red;
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,10 +1,41 @@
 <template>
   <div class="home">
-    <h3>Home</h3>
+    <HomeProjects :loading="projectsLoading" :projects="projects" />
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import HomeProjects from '@/components/HomeProjects';
+import moment from 'moment';
+
 export default {
+  components: {
+    HomeProjects
+  },
+  data() {
+    return {
+      projectsLoading: true,
+      projects: []
+    }
+  },
+  beforeMount() {
+    axios.get("https://api.github.com/users/almuz/repos?sort=updated")
+    .then(res => {
+      console.log(res.data);
+      this.projects = res.data.map(project => {
+        let newProject = {};
+        newProject.name = project.name;
+        newProject.language = project.language;
+        newProject.link = project.html_url;
+        newProject.created = moment(new Date(project.created_at)).format("YYYY-MM-DD HH:mm:ss");
+        newProject.updated = moment(new Date(project.updated_at)).format("YYYY-MM-DD HH:mm:ss");
+        return newProject;
+      })
+      setTimeout((() => {
+        this.projectsLoading = false;
+      }), 2000);
+    })
+  }
 };
 </script>
