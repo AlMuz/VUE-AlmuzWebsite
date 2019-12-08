c7e4713861638cc029e7102f9c793eb86980bf74
AlMuz <almuz@inbox.lv>
2019-12-08 13:22:29 +0200

    About page done

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="about">
-    <h3>This is an about page</h3>
+    <h3>{{ $t("aboutMePage.aboutMe") }}</h3>
+    <p v-html='$t("aboutMePage.information") '></p>
+    <i18n path="aboutMePage.informationContactMe" tag="p">
+      <router-link place="action" to='contact' tag='a'>{{ $t('aboutMePage.message') }}</router-link>
+    </i18n>
+    <h4>{{ $t("aboutMePage.techSkills") }}:</h4>
+    <ul>
+      <li v-for="(value, index) in skills" :key="index">
+        {{ value }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      skills: [
+        "PHP, Laravel, CakePHP, Pyrocms, Wordpress",
+        "HTML&CSS, Javascript, Vue.js, React, Node.js, Express.js, React Native, SASS",
+        "MySQL / MariaDB, MongoDB",
+        "Git, Docker, Heroku",
+        "Apache, Nginx",
+        "Experience with Linux"
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
