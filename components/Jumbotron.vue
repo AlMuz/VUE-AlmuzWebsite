316dacd6982b0b527d74ca832dc6c2a121aab3ef
AlMuz <almuz@inbox.lv>
2020-01-06 00:09:21 +0200

    Jumbatron added

---
+++ components/Jumbotron.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-jumbotron>
+    <b-row>
+      <b-col md="4" cols="12">
+        <b-img
+          src="~/assets/images/me.png"
+          fluid
+          alt="Responsive image"
+          rounded="circle"
+          class="mx-auto d-block"
+        />
+      </b-col>
+      <b-col md="7" offset-md="1">
+        <h1>{{ $t("jumbotron.nameAndSurname") }}</h1>
+        <h2>{{ $t("jumbotron.position") }}</h2>
+      </b-col>
+    </b-row>
+  </b-jumbotron>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+.jumbotron {
+  margin-bottom: 0!important;
+  border-radius: 0!important;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,26 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+  <b-container>
+    <b-row>
+      <b-col>
+        <Jumbotron />
+        <div class="content">
+          <transition name="slide" mode="out-in">
+            <nuxt />
+          </transition>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
+
+<script>
+import Jumbotron from '@/components/Jumbotron'
+export default {
+  components: {
+    Jumbotron
+  }
+}
+</script>
 
 <style>
 
