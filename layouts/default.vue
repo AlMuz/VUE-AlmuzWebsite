b272a734c141f419f8bf288faf9facb44ba78ecc
Aleksejs Muzičenko <aleksejs@mendo.lv>
2020-01-06 14:33:10 +0200

    Styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -34,5 +34,62 @@ export default {
 </script>
 
 <style>
-
+body {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #272643 !important;
+  background-color: #ebf1f4 !important;
+}
+.content {
+  margin: 20px;
+}
+.content h3 {
+  text-align: center;
+}
+.content h3,
+.content h4,
+.content h5 {
+  text-transform: uppercase;
+  text-decoration: underline;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+.slide-enter {
+  opacity: 0;
+}
+.slide-enter-active {
+  animation: slide-in 1s ease-out forwards;
+  transition: opacity 0.5s;
+}
+.slide-leave-active {
+  animation: slide-out 1s ease-out forwards;
+  transition: opacity 1s;
+  opacity: 0;
+}
+.slide-move {
+  transition: transform 1s;
+}
+@keyframes slide-in {
+  from {
+    transform: translateY(20px);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(20px);
+  }
+}
 </style>
