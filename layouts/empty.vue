6e079d8d48936a58f7f3d07128aeb7a5ebf67188
Aleksejs Muzičenko <aleksejs@mendo.lv>
2020-01-07 16:44:24 +0200

    Empty layout and conversation page added

---
+++ layouts/empty.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      titleTemplate: `%s - ${this.$t('meta.titleTemplate')}`
+    }
+  }
+}
+</script>
---
+++ pages/conversation.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="chat">
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'empty'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
